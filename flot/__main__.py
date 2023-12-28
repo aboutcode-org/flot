2c5a13c1c2a1994490d735bd65ebbd597ac5efd0
Philippe Ombredanne <pombredanne@nexb.com>
2023-12-28 15:21:36 +0100

    Guard run on import with a if __main__ block
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- flot/__main__.py
+++ flot/__main__.py
@@ -1,3 +1,10 @@
-from . import main
+#
+# SPDX-License-Identifier: BSD-3-clause
+# Copyright (c) nexB Inc. and contributors
+# Copyright (c) 2015, Thomas Kluyver and contributors
+# Based on https://github.com/pypa/flit/ and heavily modified
 
-main()
+if __name__ == "__main__":
+    from . import main
+
+    main()
